1c0015c4504418d9fbd46f17855df6d94791f0e6
William <68653853+will-i-amv@users.noreply.github.com>
2021-09-22 16:07:39 -0400

    Remove requirements from setup.py, there's a requirements.txt file already

--- setup.py
+++ setup.py
@@ -8,13 +8,4 @@ setup(
     license='MIT',
     url='https://github.com/will-i-amv/stock-analysis',
     packages=['stock_analysis'],
-    install_requires=[
-        'matplotlib>=3.0.2',
-        'numpy>=1.15.2',
-        'pandas>=0.23.4',
-        'pandas-datareader>=0.7.0',
-        'seaborn>=0.11.0',
-        'statsmodels>=0.11.1',
-        'mplfinance>=0.12.7a4'
-    ],
 )
