0ca3a5acfb223fa8effe5ed48259ece50cc4eee3
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-08-13 16:56:39 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='stock_analysis',
     version='0.2',
     description='Classes for technical analysis of stocks.',
-    author='William Vera',
+    author='Stefanie Molin, William Vera',
     license='MIT',
     url='https://github.com/will-i-amv/stock-analysis',
     packages=['stock_analysis'],
